fc96ea70325d68be1f916d35bd2c19873adbe34b
Amit <theamit.969@gmail.com>
2025-07-11 21:23:55 +0530

    few more qns

--- Week-02-More about JS/2.1/Easy/Q1.js
+++ Week-02-More about JS/2.1/Easy/Q1.js
@@ -4,34 +4,6 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-// function isAnagram(Str1, Str2) {
-//   const sortedStr1 = Str1.toLowerCase().split('').sort().join('');  // both strings to lowercase. This makes it case-insensitive (so "ROHAN" and "ohran" are treated the same)
-//   const sortedStr2 = Str2.toLowerCase().split('').sort().join('');
-
-//   if (sortedStr1==sortedStr2){
-//     return true;
-    
-//   }
-//   else{
-//     return false;
-    
-//   }
-// }
-// const a = isAnagram("rohan","ohran")
-// console.log(a);
-
-
-// Note:
-// String ko direct sort krne k liye koi function nahi.
-// Str ko lowercase kiye after that we use split function to split it which converts the string to array.
-// After that array ke pass sorting method hota we will sort with that. 
-// After that hm usko join krwa diye.
-// Then woh string mein convert ho jta phr usko sortedstr1 se sortedstr2 ko compare krwa diye. 
-
-//      .split('')   // Step 2: Split the string into an array of characters (e.g., 'rohan' → ['r','o','h','a','n'])
-//     .sort()      // Step 3: Sort the characters in alphabetical order (e.g., ['r','o','h','a','n'] → ['a','h','n','o','r'])
-//     .join('');   // Step 4: Join the sorted array back into a string (e.g., ['a','h','n','o','r'] → "ahnor")
-
 
 function isAnagram(str1,str2){
     const sortedStr1= str1.toLowerCase().split('').sort().join('');   // both strings to lowercase. This makes it case-insensitive (so "AMIT" and "mita" are treated the same)
