13afa69cbdf35c4cf7767eab741733e175850f41
Amit <theamit.969@gmail.com>
2025-07-14 23:32:49 +0530

    assignment_1 and added qns

---
+++ Week-02-More about JS/2.1/Assignments.md
@@ -0,0 +1,6 @@
+    Week 02 - 2.1 
+   
+
+    Try to create promisified version of setTimeout
+    Create promisified version of fetch
+    Create promisified version of fs.readFile
---
+++ Week-02-More about JS/2.1/assignment-Sollutions-1.js
@@ -0,0 +1,16 @@
+// Try to create promisified version of setTimeout
+
+function setTimeoutPromisified(ms){
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+function callback(){
+  console.log("print after 3 sseconds");
+}
+  setTimeoutPromisified(3000).then(callback);
+
+// setTimeoutPromisified(3000,).then(function callback(){    // Note - can  directly log in then() also  
+//     console.log("print after 3 seconds");
+    
+// })
